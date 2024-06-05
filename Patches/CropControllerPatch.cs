77557f7112b13295895c3f63bd4c946c238e3d31
zelzmiy <51797223+zelzmiy@users.noreply.github.com>
2024-06-05 12:25:47 -0500

    placeholder wheat structure
    while i try to figure out how to add crop structures to the game

---
+++ Patches/CropControllerPatch.cs
@@ -0,0 +1,37 @@
+﻿using FoodPlus.Items;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using static InventoryItem;
+using static FoodPlus.Items.ItemRegistery;
+
+namespace FoodPlus.Patches
+{
+    internal static class CropControllerPatch
+    {
+
+        [HarmonyPatch(typeof(CropController), nameof(CropController.CropGrowthTimes)), HarmonyPostfix]
+        public static void AddCustomCropTypes(ITEM_TYPE ___seedType, ref float __result)
+        {
+            if (___seedType is
+                ITEM_TYPE.SEED or
+                ITEM_TYPE.SEED_PUMPKIN or
+                ITEM_TYPE.SEED_COTTON or
+                ITEM_TYPE.SEED_GRAPES or
+                ITEM_TYPE.SEED_BEETROOT or
+                ITEM_TYPE.SEED_CAULIFLOWER or
+                ITEM_TYPE.SEED_HOPS or
+                ITEM_TYPE.SEED_MUSHROOM or
+                ITEM_TYPE.SEED_SOZO)
+            {
+                return;
+            }
+
+            if (___seedType == IchorSeeds) __result = 24f;
+            if (___seedType == LettuceSeeds) __result = 12f;
+            if (___seedType == OnionBulb) __result = 15f;
+            if (___seedType == TomatoSeeds) __result = 15f;
+            if (___seedType == WheatSeeds) __result = 9f;
+        }
+    }
+}
